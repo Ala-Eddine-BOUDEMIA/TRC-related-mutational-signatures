68ad057894de9a140b97e00c28b8419aaffd8c03
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2022-03-26 17:03:05 +0100

    -

--- matgen.py
+++ matgen.py
@@ -2,4 +2,4 @@ import os
 from SigProfilerMatrixGenerator.scripts import SigProfilerMatrixGeneratorFunc as matGen
 
 matrices = matGen.SigProfilerMatrixGeneratorFunc(
-	"BRCA_No_UTR", "GRCh38", "Data/BRCA/No_UTR/", plot=True)
+	"BRCA_remain", "GRCh38", "Data/BRCA/Remain/", plot=True)
