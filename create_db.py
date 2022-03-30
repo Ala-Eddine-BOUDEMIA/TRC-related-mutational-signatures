8021df4b19944fee95b57c27ac4e66bc98afb4f1
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2022-03-30 21:35:37 +0200

    relocated to extra code

--- create_db.py
+++
@@ -1,7 +0,0 @@
-import gffutils
-
-db = gffutils.create_db(
-	"Annotations/gencode-v39-annotation.gff3", 
-	dbfn='gencode_grch38.db', force=True, 
-	keep_order=True, merge_strategy='merge', 
-	sort_attribute_values=True)
