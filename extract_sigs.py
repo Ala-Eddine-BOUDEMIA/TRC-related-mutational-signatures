021fc43d3b4d506f45b7ed35d982dcb04f255225
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2022-04-04 12:55:14 +0200

    signatures 6kb window

--- extract_sigs.py
+++ extract_sigs.py
@@ -1,6 +1,6 @@
 from SigProfilerExtractor import sigpro as sig
 
-sig.sigProfilerExtractor("matrix", "Mutational_Signatures/BRCA/Keep/3UTR", 
-	"Mutational_Profiles/BRCA/Keep/3UTR/SBS/BRCA_3UTR_Keep.SBS96.all", 
+sig.sigProfilerExtractor("matrix", "Mutational_Signatures/BRCA/TSS/6kb", 
+	"Mutational_Profiles/BRCA/TSS/6kb/SBS/BRCA_TSS_6kb.SBS96.all", 
 	reference_genome = "GRCh38", opportunity_genome = "GRCh38",
 	context_type = "96", minimum_signatures = 1, maximum_signatures = 4)
