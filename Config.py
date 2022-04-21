c9c59188e5fe2f6809405960e7cc24d8eb512bc7
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2022-04-21 17:29:13 +0200

    -

--- Config.py
+++ Config.py
@@ -4,32 +4,26 @@ from pathlib import Path
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--cancer_type", 
-	metavar = "-c",
 	type = str,
 	default = "BRCA",
 	help = "Dataset to use")
 
-
 parser.add_argument("--region",
-	metavar = "-r", 
 	type = str,
 	default = "TSS",
 	help = "TSS, TTS or Remain")
 
 parser.add_argument("--gff3", 
-	metavar = "-g",
 	type = Path,
 	default = Path("Annotations/grch38.gff3"),
 	help = "GRCh38 gff3 annotation file")
 
 parser.add_argument("--all_coding_genes", 
-	metavar = "-acg",
 	type = Path,
 	default = Path("Annotations/coding_genes_hg38.bed"),
 	help = "All coding genes extracted from the gff3 file")
 
 parser.add_argument("--non_overlapping_genes", 
-	metavar = "-nog",
 	type = Path,
 	default = Path("Annotations/coding_genes_hg38_NOV_0kb.bed"),
 	help = "File containing only non-overlapping genes")
