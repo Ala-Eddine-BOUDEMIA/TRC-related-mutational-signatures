4e706726c770fd5efc76ee58375a2d69f723d824
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2022-05-04 14:45:24 +0200

    patients in common between maf and rna

---
+++ Extra_code/maf-rna-association.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+file = pd.read_csv("Data/BRCA/Original/brca.maf", header=5, sep="\t")
+
+tcga = pd.read_csv("Annotations/Metadata/TCGA/TCGA.tsv", sep="\t")
+tcga = tcga[tcga['gdc_cases.samples.sample_type']=="Primary Tumor"]
+tcga = tcga[tcga['gdc_cases.project.primary_site']=="Bladder"]
+
+rna = tcga['gdc_cases.submitter_id'].str.split('-').str[2]
+maf = file['Tumor_Sample_Barcode'].str.split("-").str[2]
+intersection = pd.Series(list(set(rna).intersection(set(maf))))
+
+print("Number of unique patients in the MAF file is: ", len(maf.unique()))
+print("Number of unique patients in the RNA-Seq file is: ", len(rna.unique()))
+print("Number of common patients between the files: ", len(intersection))
