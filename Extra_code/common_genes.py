1895901ba0ba14a5b73cd4dcee5457ad80ca7af9
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2022-05-10 10:59:11 +0200

    genes mutated at tss and tts

---
+++ Extra_code/common_genes.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+import Config
+
+cancer = Config.args.cancer_type
+tss = pd.read_csv("MAF_Analysis/" + cancer + "/Summaries/TSS/6kb/" + cancer.lower() + "-tss-6kb_geneSummary.txt", sep = "\t")
+tts = pd.read_csv("MAF_Analysis/" + cancer + "/Summaries/TTS/6kb/" + cancer.lower() + "-tts-6kb_geneSummary.txt", sep = "\t")
+
+intersection = tss.index.intersection(tts.index)
+
+print(len(intersection))
+
+pd.Series(intersection).to_csv("TSS-TTS-6kb.tsv", sep = "\t")
