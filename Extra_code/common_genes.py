8819457d1ece9cb11e7b3b01c2148d176061664d
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2022-05-10 13:28:49 +0200

    correction

--- Extra_code/common_genes.py
+++ Extra_code/common_genes.py
@@ -3,8 +3,8 @@ import pandas as pd
 import Config
 
 cancer = Config.args.cancer_type
-tss = pd.read_csv("MAF_Analysis/" + cancer + "/Summaries/TSS/6kb/" + cancer.lower() + "-tss-6kb_geneSummary.txt", sep = "\t")
-tts = pd.read_csv("MAF_Analysis/" + cancer + "/Summaries/TTS/6kb/" + cancer.lower() + "-tts-6kb_geneSummary.txt", sep = "\t")
+tss = pd.read_csv("MAF_Analysis/" + cancer + "/Summaries/TSS/6kb/" + cancer.lower() + "-tss-6kb_geneSummary.txt", index_col = 0, sep = "\t")
+tts = pd.read_csv("MAF_Analysis/" + cancer + "/Summaries/TTS/6kb/" + cancer.lower() + "-tts-6kb_geneSummary.txt", index_col = 0, sep = "\t")
 
 intersection = tss.index.intersection(tts.index)
 
